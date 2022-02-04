401c2eb7e5e3ca43011ec98bd03760a00c103790
janlingen <jan-lingen@outlook.com>
2022-02-04 15:24:50 +0100

    catching dict errors

--- information_filter.py
+++ information_filter.py
@@ -9,43 +9,43 @@ def validate_data(data):
 def get_name(symbol):
     data = get_stock_data(symbol)
     validate_data(data)
-    return data["quoteResponse"]["result"][0]["shortName"]
+    return data["quoteResponse"]["result"][0].get("shortName", "noName")
 
 
 def get_marketcap(symbol):
     data = get_stock_data(symbol)
     validate_data(data)
-    marketcap = data["quoteResponse"]["result"][0]["marketCap"]
-    currency = data["quoteResponse"]["result"][0]["currency"]
+    marketcap = data["quoteResponse"]["result"][0].get("marketCap", 0)
+    currency = data["quoteResponse"]["result"][0].get("currency", "none")
     return (marketcap, currency)
 
 
 def get_price(symbol):
     data = get_stock_data(symbol)
     validate_data(data)
-    price = data["quoteResponse"]["result"][0]["ask"]
+    price = data["quoteResponse"]["result"][0].get("ask", 0)
     if price == 0:
-        price = data["quoteResponse"]["result"][0]["preMarketPrice"]
-    currency = data["quoteResponse"]["result"][0]["currency"]
+        price = data["quoteResponse"]["result"][0].get("preMarketPrice", 0)
+    currency = data["quoteResponse"]["result"][0].get("currency", 0)
     return (price, currency)
 
 
 def get_trailing_pe(symbol):
     data = get_stock_data(symbol)
     validate_data(data)
-    return data["quoteResponse"]["result"][0]["trailingPE"]
+    return data["quoteResponse"]["result"][0].get("trailingPE", 0)
 
 
 def get_forward_pe(symbol):
     data = get_stock_data(symbol)
     validate_data(data)
-    return data["quoteResponse"]["result"][0]["forwardPE"]
+    return data["quoteResponse"]["result"][0].get("forwardPE", 0)
 
 
 def get_year_eps(symbol):
     data = get_stock_data(symbol)
-    eps = data["quoteResponse"]["result"][0]["epsCurrentYear"]
-    currency = data["quoteResponse"]["result"][0]["currency"]
+    eps = data["quoteResponse"]["result"][0].get("epsCurrentYear", 0)
+    currency = data["quoteResponse"]["result"][0].get("currency", "none")
     validate_data(data)
     return (eps, currency)
 
@@ -53,7 +53,7 @@ def get_year_eps(symbol):
 def get_year_pe(symbol):
     data = get_stock_data(symbol)
     validate_data(data)
-    return data["quoteResponse"]["result"][0]["priceEpsCurrentYear"]
+    return data["quoteResponse"]["result"][0].get("priceEpsCurrentYear", 0)
 
 
 def get_trailing_dividend_yield(symbol):
@@ -65,8 +65,8 @@ def get_trailing_dividend_yield(symbol):
 def get_two_hundred_day_average(symbol):
     data = get_stock_data(symbol)
     validate_data(data)
-    average = data["quoteResponse"]["result"][0]["twoHundredDayAverage"]
-    currency = data["quoteResponse"]["result"][0]["currency"]
+    average = data["quoteResponse"]["result"][0].get("twoHundredDayAverage", 0)
+    currency = data["quoteResponse"]["result"][0].get("currency", "none")
     return (average, currency)
 
 
@@ -75,22 +75,22 @@ def get_symbol_suggestions(suggestion):
     data = data["quotes"]
     suggestions = []
     for x in data:
-        name = x.get("shortname", "no Name")
-        symbol = x.get("symbol", "no Symbol")
+        name = x.get("shortname", "noName")
+        symbol = x.get("symbol", "noSymbol")
         suggestions.append((name, symbol))
     return suggestions
 
 
 def get_stock(symbol):
     data = get_stock_data(symbol)
-    name = data["quoteResponse"]["result"][0]["shortName"]
-    marketcap = data["quoteResponse"]["result"][0]["marketCap"]
-    price = data["quoteResponse"]["result"][0]["ask"]
+    name = data["quoteResponse"]["result"][0].get("shortName", "noName")
+    marketcap = data["quoteResponse"]["result"][0].get("marketCap", 0)
+    price = data["quoteResponse"]["result"][0].get("ask", 0)
     if price == 0:
-        price = data["quoteResponse"]["result"][0]["preMarketPrice"]
-    twoav = data["quoteResponse"]["result"][0]["twoHundredDayAverage"]
-    trailingpe = data["quoteResponse"]["result"][0]["trailingPE"]
-    currency = data["quoteResponse"]["result"][0]["currency"]
+        price = data["quoteResponse"]["result"][0].get("preMarketPrice", 0)
+    twoav = data["quoteResponse"]["result"][0].get("twoHundredDayAverage", 0)
+    trailingpe = data["quoteResponse"]["result"][0].get("trailingPE", 0)
+    currency = data["quoteResponse"]["result"][0].get("currency", "none")
     divyield = "{:.2f}".format(data["quoteResponse"]["result"][0].get(
         "trailingAnnualDividendYield", 0)*100) + " %"
     return [name, marketcap, price, twoav, trailingpe, divyield, currency]
